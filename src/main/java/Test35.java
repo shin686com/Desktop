a123d455a2954cc5bd8dfdf427ea45f4d7bb00e3
shin686com <sgs686@naver.com>
2022-06-30 21:26:56 +0900

    2022 06 30 커밋

--- src/main/java/Test35.java
+++ src/main/java/Test35.java
@@ -15,38 +15,36 @@ public class Test35 {
         int M = in.nextInt();
 
 
-        for(int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             deque.offer(i);
         }
 
         int[] seq = new int[M];
 
-        for(int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) {
             seq[i] = in.nextInt();
         }
 
 
-        for(int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) {
 
             int target_idx = deque.indexOf(seq[i]);
             int half_idx;
-            if(deque.size() % 2 == 0) {
+            if (deque.size() % 2 == 0) {
                 half_idx = deque.size() / 2 - 1;
-            }
-            else {
+            } else {
                 half_idx = deque.size() / 2;
             }
 
 
-            if(target_idx <= half_idx) {
-                for(int j = 0; j < target_idx; j++) {
+            if (target_idx <= half_idx) {
+                for (int j = 0; j < target_idx; j++) {
                     int temp = deque.pollFirst();
                     deque.offerLast(temp);
                     count++;
                 }
-            }
-            else {
-                for(int j = 0; j < deque.size() - target_idx; j++) {
+            } else {
+                for (int j = 0; j < deque.size() - target_idx; j++) {
                     int temp = deque.pollLast();
                     deque.offerFirst(temp);
                     count++;
@@ -61,4 +59,3 @@ public class Test35 {
 
     }
 }
- 
